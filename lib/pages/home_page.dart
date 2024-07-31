0d41202b8d5d61608aa5dc8056161187405b4f95
Rehan Khan <rehankhan121102@gmail.com>
2024-08-01 00:37:42 +0530

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:light_dark/components/box.dart';
+import 'package:light_dark/components/button.dart';
+import 'package:light_dark/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget
+{
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: MyBox(
+        color: Theme.of(context).colorScheme.primary,
+      child: MyButton(
+        color: Theme.of(context).colorScheme.secondary,
+        onTap:() {
+          Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+        },
+      ),
+    ),
+    ),
+    );
+  }
+}
